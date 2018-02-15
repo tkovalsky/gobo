7e8186e2dc275035f8bd4bc09b0b1b0018720b8e
Work <work@Todds-MBP.home>
2018-02-15 17:15:08 -0500

    added signup table

--- core/models.py
+++ core/models.py
@@ -15,6 +15,10 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -22,3 +26,14 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+
+class Signup(models.Model):
+    name = models.CharField(blank=False, max_length=200)
+    email = models.EmailField(primary_key=True, unique=True, blank=False, max_length=200)
+    signup_type = models.CharField(max_length=1)
+    message = models.TextField(blank=False, max_length=200)
+
+    def __str__(self):
+        return self.email
