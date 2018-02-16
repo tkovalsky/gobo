607511f0b5d3f363578ff44cc64d634f398f51a6
Work <work@Todds-MBP.home>
2018-02-15 20:59:52 -0500

    updated home

--- core/forms.py
+++ core/forms.py
@@ -5,4 +5,4 @@ from .models import Contact
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ('name', 'email',)
+        fields = ('name', 'email', 'robot')
