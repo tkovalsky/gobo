620e9e774da4923ddaf2f2c6f2ead73494d133cb
Work <work@Todds-MBP.home>
2018-02-16 07:30:24 -0500

    added core

--- core/views.py
+++ core/views.py
@@ -28,19 +28,3 @@ def signup(request):
 
 def services(request):
     return render(request, 'services.html')
-
-#def index(request):
-#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#    context = {'latest_question_list': latest_question_list}
-#    return render(request, 'core/index.html', context)
-
-#def detail(request, question_id):
-#    question = get_object_or_404(Question, pk=question_id)
-#    return render(request, 'core/detail.html', {'question': question})
-
-#def results(request, question_id):
-#    response = "You're looking at the results of question %s."
-#    return HttpResponse(response % question_id)
-
-#def vote(request, question_id):
-#    return HttpResponse("You're voting on question %s." % question_id)
